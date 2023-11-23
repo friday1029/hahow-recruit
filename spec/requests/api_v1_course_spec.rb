f05a56eaabde92ab162e4aca973595be25a4bad6
David <friday10292001@yahoo.com.tw>
2023-11-23 19:08:15 +0800

    feat: course detail rspec
    課程詳細資訊測試

--- spec/requests/api_v1_course_spec.rb
+++ spec/requests/api_v1_course_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::CoursesController, type: :request do
   before :all do
-    # 建立假課程資料
+    # 建立測試用課程資料
     # 因為 model 有檢查建立課程同時要有章節跟單元,如果直接用 create 會出錯,所以先 build 好再 save.
     rand(1..5).times.each do |i|
       # chapters 如果直接使用 build_list,會造成產生出來的 chapters 只有一組會正常有 units
@@ -35,7 +35,31 @@ RSpec.describe Api::V1::CoursesController, type: :request do
       res = JSON.parse(response.body)
       expect(res.dig('courses').sample.dig("chapters").sample.dig("units").present?).to be true
     end
+  end
 
+  describe "課程詳細資訊" do
+    it "課程詳細資訊路徑沒有問題" do
+      get api_v1_course_path(Course.all.sample)
+      expect(response).to be_successful
+    end
+
+    it "課程詳細資訊包含章節資訊" do
+      course = Course.all.sample
+      get api_v1_course_path(course)
+      res = JSON.parse(response.body)
+      res_column_names = (res.dig("course", "chapters").sample.keys - ["units"]).sort
+      chapter_column_names = Chapter.column_names.sort
+      expect(res_column_names).to eq chapter_column_names
+    end
+
+    it "章節資訊包含單元資訊" do
+      course = Course.all.sample
+      get api_v1_course_path(course)
+      res = JSON.parse(response.body)
+      res_column_names = res.dig("course", "chapters").sample.dig("units").sample.keys.sort
+      unit_column_names = Unit.column_names.sort
+      expect(res_column_names).to eq unit_column_names
+    end
   end
 
 end
